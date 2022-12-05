fe22034d2ce43e3a8b4899d8241a5d4ab6273e6d
v-abayoumy <90580037+v-abayoumy@users.noreply.github.com>
2022-12-05 08:05:21 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,5 +1,9 @@
 winget install starship --force
-winget install SourceFoundry.HackFonts --force
+Invoke-WebRequest https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Hack.zip  -OutFile "$env:TEMP\Hack.zip"
+Invoke-WebRequest https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip  -OutFile "$env:TEMP\JetBrainsMono.zip"
+Expand-Archive -LiteralPath "$env:TEMP\Hack.zip" -DestinationPath "$env:TEMP\Fonts\"
+Expand-Archive -LiteralPath "$env:TEMP\JetBrainsMono.zip" -DestinationPath "$env:TEMP\Fonts\"
+Move-Item "$env:TEMP\Fonts\*.ttf" "C:\Windows\Fonts\"
 if (-not (Get-Module -Name "Terminal-Icons")) {Install-Module Terminal-Icons -force }
 Move-Item $PROFILE "$($PROFILE)-$((Get-Date).ToString('ddMMyyyy-HHmm')).bk"
 Move-Item $PROFILE.AllUsersAllHosts "$($PROFILE.AllUsersAllHosts)-$((Get-Date).ToString('ddMMyyyy-HHmm')).bk"
