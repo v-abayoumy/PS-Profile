52f36aacc484752bb79c182bbefc6de9efd589e8
v-abayoumy <90580037+v-abayoumy@users.noreply.github.com>
2022-12-05 07:32:48 +0200

    Update install.ps1
    check if Terminal-Icons not installed before install

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
 winget install starship --force
-Install-Module Terminal-Icons -force
+if (-not (Get-Module -Name "Terminal-Icons")) {Install-Module Terminal-Icons -force }
 Move-Item $PROFILE "$($PROFILE)-$((Get-Date).ToString('ddMMyyyy-HHmm')).bk"
 Move-Item $PROFILE.AllUsersAllHosts "$($PROFILE.AllUsersAllHosts)-$((Get-Date).ToString('ddMMyyyy-HHmm')).bk"
 Copy-Item profile.ps1 $PROFILE.AllUsersAllHosts
